b24f38dd4b2c71ce971820c98aede55c77588578
xadparo <45482148+xadparo@users.noreply.github.com>
2021-08-18 11:19:13 +0900

    feat: use node_modules & swagger module apply

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,20 @@ import 'source-map-support/register'
 import 'reflect-metadata'
 
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './modules/app/app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('')
+    .setDescription('')
+    .setVersion('')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(3000)
 }
 bootstrap()
