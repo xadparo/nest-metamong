3baa4e7b8f9f93c5945c06f3ebce70cecd44a501
xadparo <45482148+xadparo@users.noreply.github.com>
2022-02-16 18:09:41 +0900

    feat: nest config 모듈 적용

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,9 +1,12 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   exports: [AppService],
   providers: [AppService],
